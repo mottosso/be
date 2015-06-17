a3d22d5190bc2446407d6bc22a9eb1131e1f239e
Marcus Ottosson <konstruktion@gmail.com>
2015-06-17 18:11:45 +0100

    Updating bash subshell

--- be/_shell.sh
+++ be/_shell.sh
@@ -1,7 +1,7 @@
 # be subshell, do not call directly.
-if ! [ -z ${BE_ENTER+x} ]; then cd $BE_DEVELOPMENTDIR; fi
+if ! [ "$BE_ENTER" = "" ]; then cd $BE_DEVELOPMENTDIR; fi
 
 # Run script
-if ! [ -z ${BE_SCRIPT+x} ]; then . $BE_SCRIPT; fi
+if ! [ "$BE_SCRIPT" = "" ];then . $BE_SCRIPT;fi
 
 bash
