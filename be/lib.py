0520738551acffa638a0c2e0cfd44f9de28fd311
Marcus Ottosson <konstruktion@gmail.com>
2015-06-17 18:00:57 +0100

    Safeguarding be ls against permission issues

--- be/lib.py
+++ be/lib.py
@@ -29,14 +29,18 @@ def isproject(path):
 
     """
 
-    if os.path.basename(path)[0] in (".", "_"):
+    try:
+        if os.path.basename(path)[0] in (".", "_"):
+            return False
+        if not os.path.isdir(path):
+            return False
+        if not any(fname in os.listdir(path)
+                   for fname in ("templates.yaml",
+                                 "inventory.yaml")):
+            return False
+    except:
         return False
-    if not os.path.isdir(path):
-        return False
-    if not any(fname in os.listdir(path)
-               for fname in ("templates.yaml",
-                             "inventory.yaml")):
-        return False
+
     return True
 
 
